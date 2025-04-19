b92e3eeb2fb7b8715b8d7608be54025a20e152c0
John Gorman <967905+gorman-john-m@users.noreply.github.com>
2025-04-19 13:36:24 -0700

    Initial Commit

---
+++ HoverShot/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HoverShot
+//
+//  Created by John Gorman on 4/19/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
